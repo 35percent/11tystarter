7629e05d2c3a8a2e2f6641d73f5e246c4a568064
regenwatch <router@router.lan>
2022-02-25 21:23:25 +0000

    passthorough

--- .eleventy.js
+++ .eleventy.js
@@ -30,7 +30,7 @@ module.exports = function (eleventyConfig) {
   });
 
   // Copy Image Folder to /_site
-  eleventyConfig.addPassthroughCopy("./src/static/img");
+  eleventyConfig.addPassthroughCopy("img");
 
   // Copy favicon to route of /_site
   eleventyConfig.addPassthroughCopy("./src/favicon.ico");

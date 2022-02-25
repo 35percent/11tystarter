238469a98c3d787891b01cf9599f0fb0c599a7dd
unknown <a_glasspool@hotmail.com>
2022-02-25 13:20:37 +0000

    added img plugin

--- .eleventy.js
+++ .eleventy.js
@@ -2,10 +2,36 @@ const yaml = require("js-yaml");
 const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const htmlmin = require("html-minifier");
+const Image = require("@11ty/eleventy-img");
 
 module.exports = function (eleventyConfig) {
   // Disable automatic use of your .gitignore
   eleventyConfig.setUseGitIgnore(false);
+
+    // --- START, eleventy-img
+    async function imageShortcode(src, alt, sizes) {
+      let metadata = await Image(src, {
+        widths: [300, 600],
+        formats: ["avif", "jpeg"]
+      });
+    
+      let imageAttributes = {
+        alt,
+        sizes,
+        loading: "lazy",
+        decoding: "async",
+      };
+    
+      // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+      return Image.generateHTML(metadata, imageAttributes);
+    }
+    
+    module.exports = function(eleventyConfig) {
+      eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+      eleventyConfig.addLiquidShortcode("image", imageShortcode);
+      eleventyConfig.addJavaScriptFunction("image", imageShortcode);
+    };
+    // --- END, eleventy-img
 
   // Merge data instead of overriding
   eleventyConfig.setDataDeepMerge(true);

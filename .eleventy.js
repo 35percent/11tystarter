406001b5450ae1917f311a58e042b8b86598b7f2
regenwatch <router@router.lan>
2022-02-25 21:28:34 +0000

    src

--- .eleventy.js
+++ .eleventy.js
@@ -30,7 +30,7 @@ module.exports = function (eleventyConfig) {
   });
 
   // Copy Image Folder to /_site
-  eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPassthroughCopy("src/img");
 
   // Copy favicon to route of /_site
   eleventyConfig.addPassthroughCopy("./src/favicon.ico");

b6163ca5b94519ab6b05b1c85a9ce9e9f91d9cb9
regenwatch <a_glasspool@hotmail.com>
2022-02-26 21:09:08 +0000

    fuckk your stupid image plugin

--- .eleventy.js
+++ .eleventy.js
@@ -2,39 +2,14 @@ const yaml = require("js-yaml");
 const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const htmlmin = require("html-minifier");
-const Image = require("@11ty/eleventy-img");
-
-function imageShortcode(src, cls, alt, sizes, widths) {
-  let options = {
-    urlPath: "src/img/",
-    outputDir: "./_site/img/",
-    widths: widths,
-    formats: ['jpeg'],
-  };
-
-  // generate images, while this is async we don’t wait
-  Image(src, options);
-
-  let imageAttributes = {
-    class: cls,
-    alt,
-    sizes,
-    loading: "lazy",
-    decoding: "async",
-  };
-  // get metadata even the images are not fully generated
-  let metadata = Image.statsSync(src, options);
-  return Image.generateHTML(metadata, imageAttributes);
-}
-
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addNunjucksShortcode("image", imageShortcode);
-}
 
 module.exports = function (eleventyConfig) {
   // Disable automatic use of your .gitignore
   eleventyConfig.setUseGitIgnore(false);
-  
+
+  // Merge data instead of overriding
+  eleventyConfig.setDataDeepMerge(true);
+
   // human readable date
   eleventyConfig.addFilter("readableDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
@@ -58,7 +33,7 @@ module.exports = function (eleventyConfig) {
   });
 
   // Copy Image Folder to /_site
-  eleventyConfig.addPassthroughCopy("src/img");
+  eleventyConfig.addPassthroughCopy("./src/static/img");
 
   // Copy favicon to route of /_site
   eleventyConfig.addPassthroughCopy("./src/favicon.ico");

da924ca455c9b33881b35909d9bce46d1a1cab67
regenwatch <a_glasspool@hotmail.com>
2022-02-27 18:08:02 +0000

    pjdj

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,37 @@ module.exports = function (eleventyConfig) {
   // Disable automatic use of your .gitignore
   eleventyConfig.setUseGitIgnore(false);
 
+  // --- START, eleventy-img
+  function imageShortcode(src, alt, sizes="(min-width: 1024px) 100vw, 50vw") {
+    console.log(`Generating image(s) from:  ${src}`)
+    let options = {
+      widths: [600, 900, 1500],
+      formats: ["webp", "jpeg"],
+      urlPath: "/images/",
+      outputDir: "./_site/images/",
+      filenameFormat: function (id, src, width, format, options) {
+        const extension = path.extname(src)
+        const name = path.basename(src, extension)
+        return `${name}-${width}w.${format}`
+      }
+    }
+
+    // generate images
+    Image(src, options)
+
+    let imageAttributes = {
+      alt,
+      sizes,
+      loading: "lazy",
+      decoding: "async",
+    }
+    // get metadata
+    metadata = Image.statsSync(src, options)
+    return Image.generateHTML(metadata, imageAttributes)
+  }
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+  // --- END, eleventy-img
+
   // Merge data instead of overriding
   eleventyConfig.setDataDeepMerge(true);
 
